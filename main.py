e3993645316ad67a2abbbe97e2d9fb3f888d0b42
NickMallay <nickmallay@hotmail.com>
2024-12-26 20:51:48 -0500

    Moved x-to-quit functionality into main loop as was intended

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame
 from constants import *
 
 def main():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
